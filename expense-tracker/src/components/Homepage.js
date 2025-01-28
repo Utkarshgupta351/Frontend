64a1d1179e988b7a8d159910c4a2a837d6e17fd9
Utkarshgupta351 <utkarshgupta351@gmail.com>
2025-01-28 22:15:47 +0530

    Homepage for expense tracker

---
+++ expense-tracker/src/components/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import img1 from '../asset/img/dashboard.jpg'
+function Homepage() {
+  return (
+    <div className='container'>
+        <div className='leftcontainer'>
+            <p></p>
+            <img src={img1} />
+        </div>
+        <div className='rightcontainer'>
+            <p></p>
+        </div>
+    
+
+    </div>
+  )
+}
+
+export default Homepage
