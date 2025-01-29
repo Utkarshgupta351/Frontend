656269cbd72a7790ff40437e316318adbdc9e0e3
Utkarshgupta351 <utkarshgupta351@gmail.com>
2025-01-29 22:08:25 +0530

    Register Form Create

--- expense-tracker/src/components/Header.js
+++ expense-tracker/src/components/Header.js
@@ -3,6 +3,7 @@ import logo from '../asset/img/logo.jpg'
 function Header() {
   return (
     <div className='header'>
+      
         <img src={logo}/>
         <h2>Expense Tracker</h2>
     </div>
--- expense-tracker/src/components/Homepage.js
+++ expense-tracker/src/components/Homepage.js
@@ -4,13 +4,66 @@ function Homepage() {
   return (
     <div className='container'>
         <div className='leftcontainer'>
-            <p></p>
+        <p>Welcome to Expense Tracker
+      </p>
+      <p>Manage all your expenses here</p>
             <img src={img1} />
         </div>
         <div className='rightcontainer'>
-            <p></p>
+            <div className='formcard'>
+          <h2 style={{alignSelf:'container'}}>Register</h2>
+          
+          <div>
+            <label>
+              Name
+            </label>
+            <input 
+            required
+            placeholder='Enter Your Name'
+            type='text'
+            />
+          </div>
+          
+          <div>
+            <label>
+              Email
+            </label>
+            <input 
+            required
+            placeholder='Enter Your Email'
+            type='email'
+            />
+          </div>
+          
+          <div>
+            <label>
+              Password
+            </label>
+            <input 
+            required
+            placeholder='Enter Your Password'
+            type='password'
+            />
+          </div>
+          
+          <div>
+            <label>
+              Confirm Password
+            </label>
+            <input 
+            required
+            placeholder='Confirm Your Password'
+            type='password'
+            />
+          </div>
+          
+          <div>
+             <button>Submit</button>
+          </div>
+            </div>
+            
         </div>
-    
+      
 
     </div>
   )
